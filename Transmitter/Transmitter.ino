bd2df26f5fe321ac684b96cf2641317cb6115317
rahultiwarionline <rahultiwarionline@gmail.com>
2016-01-23 22:45:54 -0500

    Created CRC stuff
    Doesn't work with structs (yet)

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,31 +1,55 @@
 #include <util/crc16.h>
+#define offsetof(st, m) ((size_t)(&((st *)0)->m))
+//Function Declaration
+
+byte crc8(const byte *packet);
 
 void setup() {
   // put your setup code here, to run once:
-struct DataPacket
+  Serial.begin(9600);
+}
+typedef struct DataPacket
 {
  uint8_t Altitude;
  uint8_t Temp;
  uint8_t IMU;
  uint8_t GPS;
- uint8_t Pressure; 
+ uint8_t Pressure;
+ uint8_t Pressure1; 
  uint16_t CRC;
-}
+} DataPacket;
+
+DataPacket data;
 
 
-}
-void loop() {
 
-
-DataPacket test
+byte crc(struct DataPacket * orig, int len)
+{
+  unsigned char * raw = (unsigned char *)malloc(len);
+  memcpy(raw, &orig, len);
+  int crc = 0b1101;
+  int c = 4;
+  int i;
+  raw = raw << c-1;
+  for(i = len+c-1; i >= c; i--)
   {
-    test.Altitude = 1;
-    test.Temp = 1;
-    test.=IMU = 1;
-    test.GPS = 1;
-    test.Pressure = 1;
-    test.CRC = MakeCRC(&test);
-  }
+    if((raw >> (i-1)) % 2)
+    {
+      raw ^= crc << (i-c);
+    }
+  }  
+  return raw;
+     
+}
+
+
+
+
+void loop() {
+  
+ 
+  
+  Serial.print(crc(*data, offsetof(DataPacket, CRC)*8));
   
   // put your main code here, to run repeatedly:
   tone(8, 1200, 1000);
